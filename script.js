f420d9c9a1f340688c175f8eedac2ae56911c10a
Egor Filipas <filipasegor@gmail.com>
2021-04-20 01:37:35 +0300

    Third-sprint

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 
-const dropDown = document.querySelectorAll('.dropDown');
-const list = document.querySelector('.hidden-ul');
-const button = document.getElementById('openListButton')
-console.log(dropDown)
+  const list = document.querySelector('.hidden-ul');
+  const openButton = document.getElementById('openListButton');
 
-let state = "none"
+  let state = "none";
 
 
-button.addEventListener('click', function() {
-  if (state == "none") {
-    list.style.display = "block"
-    state = "block"
-  } else {
-    list.style.display = "none"
-    state = "none"
+  if(openButton){
+    openButton.addEventListener('click', function() {
+      if (state == "none") {
+        list.style.display = "block"
+        state = "block"
+      } else {
+        list.style.display = "none"
+        state = "none"
+      }
+    });
+  };
+
+  const dropDownFirst = document.getElementById('dropdown-par-first');
+  const dropDownSecond = document.getElementById('dropdown-par-second');
+  const onClickFirst = document.getElementById('onClickFirst');
+  const onClickSecond = document.getElementById('onClickSecond');
+
+
+  if (onClickFirst) {
+    onClickFirst.addEventListener('click', function(){
+      if (state == "none") {
+        dropDownFirst.style.display = "block"
+        state = "block"
+      } else {
+        dropDownFirst.style.display = "none"
+        state = "none"
+      }
+    })
   }
-})
+
+  if (onClickSecond) {
+    onClickSecond.addEventListener('click', function(){
+      if (state == "none") {
+        dropDownSecond.style.display = "block"
+        state = "block"
+      } else {
+        dropDownSecond.style.display = "none"
+        state = "none"
+      }
+    })
+  }
